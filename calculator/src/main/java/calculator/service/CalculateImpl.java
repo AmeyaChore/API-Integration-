3f0445d6b609be1b0c457db0fb8840bab3f3d3e4
Chore <ameyachore475@gmail.com>
2023-10-18 12:21:01 +0530

    first commit

--- calculator/src/main/java/calculator/service/CalculateImpl.java
+++ calculator/src/main/java/calculator/service/CalculateImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
 import org.springframework.web.server.ResponseStatusException;
 
 
@@ -12,10 +13,24 @@ import org.springframework.web.server.ResponseStatusException;
 @Slf4j
 public class CalculateImpl implements CalculateService {
 
+//    OkHttpClient client = new OkHttpClient();
+//
+//    MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+//    RequestBody body = RequestBody.create(mediaType, "input=sqrt(5%5E2%20-%204%5E2)");
+//    Request request = new Request.Builder()
+//            .url("https://wolframalphavolodimir-kudriachenkov1.p.rapidapi.com/createQuery")
+//            .post(body)
+//            .addHeader("content-type", "application/x-www-form-urlencoded")
+//            .addHeader("X-RapidAPI-Key", "1445e32cf6msh406aa94fc761893p1c4711jsn9dfd59ed83af")
+//            .addHeader("X-RapidAPI-Host", "WolframAlphavolodimir-kudriachenkoV1.p.rapidapi.com")
+//            .build();
+//
+//    Response response = client.newCall(request).execute();
 
-    private static final String url="https://corona-virus-world-and-india-data.p.rapidapi.com/api";
+
+    private static final String url="https://wolframalphavolodimir-kudriachenkov1.p.rapidapi.com/";
     private static final String X_RapidAPI_Key="1445e32cf6msh406aa94fc761893p1c4711jsn9dfd59ed83af";
-    private static final String X_RapidAPI_Host="corona-virus-world-and-india-data.p.rapidapi.com";
+    private static final String X_RapidAPI_Host="WolframAlphavolodimir-kudriachenkoV1.p.rapidapi.com";
 
     private RestTemplate restTemplate;
 
@@ -27,21 +42,28 @@ public class CalculateImpl implements CalculateService {
     @Override
     public Object getAnswer(String expression) {
 
-        //String dynamicUrl=url+expression;
+        String dynamicUrl=url+expression;
         try{
             HttpHeaders headers=new HttpHeaders();
             headers.set("X-RapidAPI-Key", X_RapidAPI_Key);
             headers.set("X-RapidAPI-Host", X_RapidAPI_Host);
 
-            ResponseEntity<String> response=restTemplate.exchange(url, HttpMethod.GET,new HttpEntity<>(headers),String.class);
+            ResponseEntity<String> response=restTemplate.exchange(dynamicUrl, HttpMethod.GET,new HttpEntity<>(headers),String.class);
             log.info("Output form rapidAPI:{}",response.getBody());
             return response.getBody();
+        }
+        catch(MethodArgumentConversionNotSupportedException argumentConversionNotSupportedException){
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "invalid input",
+                    argumentConversionNotSupportedException
+            );
         }
         catch(Exception e){
             log.error("Something went wrong while getting value from RapidAPI",e);
             throw new ResponseStatusException(
                     HttpStatus.INTERNAL_SERVER_ERROR,
-                    "Exception while calling endpoint of RapidAPI for corona",
+                    "something went wrong",
                     e
             );
         }
